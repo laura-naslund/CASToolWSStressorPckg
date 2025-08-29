4330297cce85785f9b959be183fc8a12f668e231
Laura Naslund <Naslund.Laura@epa.gov>
2025-08-29 10:44:52 -0400

    update description add README

--- R/retrieve_stressor_data.R
+++ R/retrieve_stressor_data.R
@@ -14,13 +14,13 @@ retrieve_stressor_data <- function(State){
     stringr::str_subset(paste0(stateAbb, "_"))
 
   # Use pblapply for parallel processing with progress bar
-  ret_data_list <- pblapply(files, function(file) {
+  ret_data_list <- pbapply::pblapply(files, function(file) {
     data(list = file, package = "CASToolWSStressorPckg", envir = environment())
     get(file, envir = environment())
   })
 
   # Bind rows of all data frames in the list
-  ret_data <- bind_rows(ret_data_list)
+  ret_data <- dplyr::bind_rows(ret_data_list)
 
   return(ret_data)
 
